620f2b54c82efcd7c96327488052d07d818eab67
Charan-kommi <saicharanchowdary3@gmail.com>
2025-04-09 19:27:24 -0400

    Update LoginActivity.kt

--- LoginActivity.kt
+++ LoginActivity.kt
@@ -1,37 +1,37 @@
-package dev.jord.todo
+package org.taskflow.app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.ui.AppBarConfiguration
 import dagger.hilt.android.AndroidEntryPoint
-import dev.jord.todo.databinding.ActivityLoginBinding
-import dev.jord.todo.ui.auth.WelcomeFragment
+import org.taskflow.app.databinding.ActivityLoginBinding
+import org.taskflow.app.ui.auth.EntryFragment
 
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityLoginBinding
+    private lateinit var navigationSettings: AppBarConfiguration
+    private lateinit var loginBinding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        loginBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(loginBinding.root)
 
-        setSupportActionBar(binding.toolbar)
-        binding.toolbar.setNavigationOnClickListener {
-            loadFragment(WelcomeFragment())
+        setSupportActionBar(loginBinding.toolbar)
+
+        loginBinding.toolbar.setNavigationOnClickListener {
+            displayFragment(EntryFragment())
         }
 
-        loadFragment(WelcomeFragment())
+        displayFragment(EntryFragment())
     }
 
-    private fun loadFragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
+    private fun displayFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
             .replace(R.id.container, fragment)
             .commit()
-    }
+    }
 }

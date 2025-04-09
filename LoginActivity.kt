a7a0b665a5ea60c3b015f9d6031c06a2d4e269ff
Darshil Patel <67738564+pateldarshil8@users.noreply.github.com>
2025-04-09 19:22:26 -0400

    Add files via upload

---
+++ LoginActivity.kt
@@ -0,0 +1,37 @@
+package dev.jord.todo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.ui.AppBarConfiguration
+import dagger.hilt.android.AndroidEntryPoint
+import dev.jord.todo.databinding.ActivityLoginBinding
+import dev.jord.todo.ui.auth.WelcomeFragment
+
+@AndroidEntryPoint
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        binding.toolbar.setNavigationOnClickListener {
+            loadFragment(WelcomeFragment())
+        }
+
+        loadFragment(WelcomeFragment())
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
+    }
+}

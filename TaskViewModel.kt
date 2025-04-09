2cd0b3dd82c703988dc486377e03be8718be3fc7
Charan-kommi <saicharanchowdary3@gmail.com>
2025-04-09 19:33:30 -0400

    Update TaskViewModel.kt

--- TaskViewModel.kt
+++ TaskViewModel.kt
@@ -1,94 +1,91 @@
-package dev.jord.todo.ui.home
+package org.taskflow.app.ui.home
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dev.jord.todo.data.model.Task
-import dev.jord.todo.data.model.User
-import dev.jord.todo.data.repository.TaskRepository
-import dev.jord.todo.util.UiState
+import org.taskflow.app.data.model.Task
+import org.taskflow.app.data.model.User
+import org.taskflow.app.data.repository.TaskRepository
+import org.taskflow.app.util.UiState
 import javax.inject.Inject
 
 @HiltViewModel
-class TaskViewModel @Inject constructor(
-    val repository: TaskRepository
-): ViewModel() {
+class TaskManagerViewModel @Inject constructor(
+    private val taskRepo: TaskRepository
+) : ViewModel() {
 
-    private val TAG = "TaskViewModel"
-    private val _addTask = MutableLiveData<UiState<Pair<Task,String>>>()
-    val addTask: LiveData<UiState<Pair<Task,String>>>
-        get() = _addTask
-    private val _updateTask = MutableLiveData<UiState<Pair<Task,String>>>()
-    val updateTask: LiveData<UiState<Pair<Task,String>>>
-        get() = _updateTask
-    private val _doneTask = MutableLiveData<UiState<Pair<Task,String>>>()
-    val doneTask: LiveData<UiState<Pair<Task,String>>>
-        get() = _doneTask
-    private val _deleteTask = MutableLiveData<UiState<Pair<Task,String>>>()
-    val deleteTask: LiveData<UiState<Pair<Task,String>>>
-        get() = _deleteTask
-    private val _storeTasks = MutableLiveData<UiState<String>>()
-    val storeTasks: LiveData<UiState<String>>
-        get() = _storeTasks
-    private val _getTask = MutableLiveData<UiState<Pair<Task,String>>>()
-    val getTask: LiveData<UiState<Pair<Task,String>>>
-        get() = _getTask
-    private val _getTasks = MutableLiveData<UiState<List<Task>>>()
-    val getTasks: MutableLiveData<UiState<List<Task>>>
-        get() = _getTasks
+    private val _taskCreation = MutableLiveData<UiState<Pair<Task, String>>>()
+    val taskCreation: LiveData<UiState<Pair<Task, String>>> get() = _taskCreation
 
-    private val _tasks = MutableLiveData<UiState<List<Task>>>()
-    val tasks: LiveData<UiState<List<Task>>>
-        get() = _tasks
+    private val _taskUpdate = MutableLiveData<UiState<Pair<Task, String>>>()
+    val taskUpdate: LiveData<UiState<Pair<Task, String>>> get() = _taskUpdate
 
-    fun addTask(task: Task) {
-        _addTask.value = UiState.Loading
-        repository.addTask(task) {
-            _addTask.value = it
+    private val _taskToggleDone = MutableLiveData<UiState<Pair<Task, String>>>()
+    val taskToggleDone: LiveData<UiState<Pair<Task, String>>> get() = _taskToggleDone
+
+    private val _taskDeletion = MutableLiveData<UiState<Pair<Task, String>>>()
+    val taskDeletion: LiveData<UiState<Pair<Task, String>>> get() = _taskDeletion
+
+    private val _taskStorage = MutableLiveData<UiState<String>>()
+    val taskStorage: LiveData<UiState<String>> get() = _taskStorage
+
+    private val _fetchedTask = MutableLiveData<UiState<Pair<Task, String>>>()
+    val fetchedTask: LiveData<UiState<Pair<Task, String>>> get() = _fetchedTask
+
+    private val _taskFetchList = MutableLiveData<UiState<List<Task>>>()
+    val taskFetchList: MutableLiveData<UiState<List<Task>>> get() = _taskFetchList
+
+    private val _allTasks = MutableLiveData<UiState<List<Task>>>()
+    val allTasks: LiveData<UiState<List<Task>>> get() = _allTasks
+
+    fun createTask(task: Task) {
+        _taskCreation.value = UiState.Loading
+        taskRepo.addTask(task) { result ->
+            _taskCreation.value = result
         }
     }
 
-    fun updateTask(task: Task) {
-        _updateTask.value = UiState.Loading
-        repository.updateTask(task) {
-            _updateTask.value = it
+    fun modifyTask(task: Task) {
+        _taskUpdate.value = UiState.Loading
+        taskRepo.updateTask(task) { result ->
+            _taskUpdate.value = result
         }
     }
 
-    fun doneTask(task: Task) {
-        _doneTask.value = UiState.Loading
+    fun toggleTaskCompletion(task: Task) {
+        _taskToggleDone.value = UiState.Loading
         task.completed = !task.completed
-        repository.updateTask(task) {
-            _doneTask.value = it
+        taskRepo.updateTask(task) { result ->
+            _taskToggleDone.value = result
         }
     }
 
-    fun deleteTask(task: Task) {
-        _deleteTask.value = UiState.Loading
-        repository.deleteTask(task) {
-            _deleteTask.value = it
+    fun removeTask(task: Task) {
+        _taskDeletion.value = UiState.Loading
+        taskRepo.deleteTask(task) { result ->
+            _taskDeletion.value = result
         }
     }
 
-   fun getTask(id: String) {
-        _getTask.value = UiState.Loading
-        repository.getTask(id) {
-            _getTask.value = it
+    fun fetchTaskById(taskId: String) {
+        _fetchedTask.value = UiState.Loading
+        taskRepo.getTask(taskId) { result ->
+            _fetchedTask.value = result
         }
     }
 
-    fun getTasks(user: User?) {
-        _tasks.value = UiState.Loading
-        repository.getTasks(user) {
-            _tasks.value = it
+    fun fetchUserTasks(user: User?) {
+        _allTasks.value = UiState.Loading
+        taskRepo.getTasks(user) { result ->
+            _allTasks.value = result
         }
     }
 
-    fun storeTasks(tasks: List<Task>) {
-        _storeTasks.value = UiState.Loading
-        repository.storeTasks(tasks) {
-            _storeTasks.value = it
-        }
-    }
+    fun saveTaskList(tasks: List<Task>) {
+        _taskStorage.value = UiState.Loading
+        taskRepo.storeTasks(tasks) { result ->
+            _taskStorage.value = result
+        }
+    }
 }
